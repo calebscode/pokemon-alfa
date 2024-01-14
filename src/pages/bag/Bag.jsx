7ca2088142e718ee13c0afc18cf9f57a0a7a6d4f
Caleb O'Malley <calebomalley@ufl.edu>
2024-01-14 18:46:16 -0500

    added money display + add button

--- src/pages/bag/Bag.jsx
+++ src/pages/bag/Bag.jsx
@@ -1,16 +1,28 @@
 import useFetch from "../../../lib/useFetch";
-import { Container } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import ItemList from "./ItemList";
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 
 export default function Bag() {
-  const url = "http://localhost:3000/users/1"
-  const { error, isPending, data } = useFetch(url)
+  const url = "http://localhost:3000/users/1";
+  const { error, isPending, data } = useFetch(url);
 
   return (
     <Container className="bag">
-      { error && <div className="error">Error: { error }</div> }
-      { isPending && <div>Loading...</div> }
-      { data && <ItemList items={data.trainerData.bag.items} /> }
+      {error && <div className="error">Error: {error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && (
+        <>
+          <Container className="bag-header" sx={{mt:"0.5rem", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+          <Box className="bag-money" sx={{display:"flex", alignItems:"center", }}>
+            <MonetizationOnIcon sx={{mr:"0.25rem"}}/>
+            <Typography variant="h6">{data.trainerData.bag.money}</Typography>
+          </Box>
+          <Button size="small" variant="contained" color="success">Add</Button>
+          </Container>
+          <ItemList items={data.trainerData.bag.items} />
+        </>
+      )}
     </Container>
   );
 }
--- src/pages/bag/ItemList.jsx
+++ src/pages/bag/ItemList.jsx
@@ -1,11 +1,20 @@
+import { Box, Container, Divider, Grid, Typography } from "@mui/material";
 import BagItem from "./BagItem";
 
 export default function ItemList({ items }) {
   return (
+
     <div className="item-list">
+    <Container sx={{display:"flex", justifyContent:"space-between"}}>
+      <Typography variant="h6">Item</Typography>
+      <Typography variant="h6">Amount</Typography>
+    </Container>
+    <Divider/>
+    <Container className="items">
       {items.map((item) => (
           <BagItem key={item.name} item={item} />
         ))}
+    </Container>
     </div>
   )
 }
